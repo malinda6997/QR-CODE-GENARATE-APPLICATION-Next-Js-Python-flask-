0b51a0b255f00e53e4adaeec83f324250f5c65cb
Malinda Prabath <malindaprabath876@gmail.com>
2025-06-28 23:05:09 +0530

    add home route to display backend status message

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,6 +4,10 @@ import io
 
 main = Blueprint("main", __name__)
 
+@main.route("/", methods=["GET"])
+def home():
+    return "<h1>QR Code Generator Backend is running</h1>"
+
 @main.route("/generate", methods=["POST"])
 def generate_qr():
     try:
@@ -12,7 +16,6 @@ def generate_qr():
             return jsonify({"error": "No text provided"}), 400
 
         qr = qrcode.make(data)
-
         buffer = io.BytesIO()
         qr.save(buffer, format="PNG")
         buffer.seek(0)
